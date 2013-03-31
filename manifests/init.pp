f81242f314cbc26cd3d352e424b5334690a0c59c
cynipe <cynipe@gmail.com>
2013-03-31 15:42:44 +0900

    Fix syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@ class keyremap4macbook(
 
   package { 'KeyRemap4MacBook':
     ensure => $ensure,
-    source => 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg'
-    provider => 'pkgdmg'
+    source => 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg',
+    provider => 'pkgdmg',
   }
 
 }
