ea46ae8f326033c2f9e0ca47129392278a40e076
cynipe <cynipe@gmail.com>
2013-03-31 14:54:45 +0900

    KeyRemap4Macbookをインストール出来るようにした

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class keyremap4macbook(
+  $ensure = present
+) {
+
+  package { 'KeyRemap4MacBook':
+    ensure => $ensure,
+    source => 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg'
+    provider => 'pkgdmg'
+  }
+
+}
